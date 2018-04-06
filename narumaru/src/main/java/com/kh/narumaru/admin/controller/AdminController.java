8062c265fcb60465f7e6da514da7502d4f7eef56
KJY <KJY@DESKTOP-TEBTTSC>
2018-04-06 17:29:27 +0900

    마루 및 로그인관리 뷰 업로드

---
+++ narumaru/src/main/java/com/kh/narumaru/admin/controller/AdminController.java
@@ -0,0 +1,42 @@
+package com.kh.narumaru.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("loginUser")
+public class AdminController {
+
+	@RequestMapping(value="adMain.ad")
+	public String showAdminMainView(){
+		   
+		return "admin/adMain";
+	}
+	
+	
+	@RequestMapping(value="adMemberView.ad")
+	public String showAdminMemberView(){
+		   
+		return "admin/adMemberView";
+	}
+	
+	@RequestMapping("adNaruView.ad")
+	public String showAdminNaruView(){
+		   
+		return "admin/adNaruView";
+	}
+	
+	@RequestMapping("adMaruView.ad")
+	public String showmAdminMaruView(){
+		
+		return "admin/adMaruView";
+	}
+	
+	@RequestMapping("adLoginManagView.ad")
+	public String showmAdminLoginManagementView(){
+		
+		return "admin/adLoginManagView";
+	}
+	
+}
