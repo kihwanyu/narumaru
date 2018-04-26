d9f68695b40cff564e21d500e56a554ba4f7bb25
heizein <apqkfjs@naver.com>
2018-04-26 17:39:13 +0900

    카테고리 가완성

--- narumaru/src/main/java/com/kh/narumaru/naru/model/service/NaruService.java
+++ narumaru/src/main/java/com/kh/narumaru/naru/model/service/NaruService.java
@@ -1,11 +1,18 @@
 package com.kh.narumaru.naru.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 
 import com.kh.narumaru.naru.model.exception.NaruException;
+import com.kh.narumaru.naru.model.vo.Category;
 
 public interface NaruService {
 
 	void insertCategory(String s, int nmno) throws NaruException;
+
+	ArrayList<Category> selectCategoryList(int nmno);
+
+	void disableCategory(String caName, int nmno);
 
 }
