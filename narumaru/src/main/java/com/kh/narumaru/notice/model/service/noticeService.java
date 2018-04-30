2f20f93eb3ffaec25c0ba56f9ab28f4d2347bbda
Joonhyun <Joonhyun@DESKTOP-UKOTQET>
2018-04-30 19:26:28 +0900

    에베베베베

--- narumaru/src/main/java/com/kh/narumaru/notice/model/service/noticeService.java
+++ narumaru/src/main/java/com/kh/narumaru/notice/model/service/noticeService.java
@@ -2,6 +2,7 @@ package com.kh.narumaru.notice.model.service;
 
 import java.util.ArrayList;
 
+import com.kh.narumaru.notice.Exception.NoticeUpdateException;
 import com.kh.narumaru.notice.model.vo.Notice;
 
 public interface noticeService {
@@ -9,5 +10,11 @@ public interface noticeService {
 	ArrayList<Notice> noticeSelectList(Notice n);
 
 	Notice showNoticeDetailView(int bno);
+
+	ArrayList<Notice> faqSelectList(Notice n);
+
+	
+
+	void updateNoticeCommit(Notice n) throws NoticeUpdateException;
 
 }
