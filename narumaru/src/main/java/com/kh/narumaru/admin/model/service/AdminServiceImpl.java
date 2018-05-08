028c6f46ddbd299377566e018313820ffc58fc0c
kihwanYu <kihwanYu@DESKTOP-MHBOHN0>
2018-05-08 16:15:36 +0900

    정리

--- narumaru/src/main/java/com/kh/narumaru/admin/model/service/AdminServiceImpl.java
+++ narumaru/src/main/java/com/kh/narumaru/admin/model/service/AdminServiceImpl.java
@@ -117,7 +117,7 @@ public class AdminServiceImpl implements AdminService {
 	public ArrayList<Notice> adminAnswer(Notice n) {
 		
 		return ad.adminAnswer(n);
-
+	}
 	public ArrayList selectChannel() {
 		ArrayList selectChannel = ad.selectChannel();
 		System.out.println("채널 : " + selectChannel);
